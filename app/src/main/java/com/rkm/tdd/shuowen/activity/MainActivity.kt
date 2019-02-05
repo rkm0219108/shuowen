ad0640e864fe2f5fc33d0510956784453934f13a
Jianmei Li <rkm0219108@gmail.com>
2019-02-05 14:48:17 +0900

    Add search on word list screen

--- app/src/main/java/com/rkm/tdd/shuowen/activity/MainActivity.kt
+++ app/src/main/java/com/rkm/tdd/shuowen/activity/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.rkm.tdd.shuowen.activity
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.rkm.tdd.shuowen.R
@@ -16,8 +18,8 @@ class MainActivity : AppCompatActivity(), Injectable {
 
     private val adapter = WordItemAdapter()
 
-    @Inject
-    lateinit var viewModelFactory: ViewModelProvider.Factory
+    @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,12 +28,32 @@ class MainActivity : AppCompatActivity(), Injectable {
 
         word_list.adapter = adapter
 
-        val viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
+        viewModel = ViewModelProviders.of(this, viewModelFactory).get(MainViewModel::class.java)
         viewModel.wordItems.observe(this) {
             if (it == null) return@observe
 
             adapter.items = it
         }
         viewModel.search.value = ""
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menu ?: return false
+
+        menuInflater.inflate(R.menu.menu_main, menu)
+        val item = menu.findItem(R.id.menu_search)
+        val searchView: SearchView = item.actionView as SearchView
+        searchView.queryHint = getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                viewModel.search.value = newText
+                return false
+            }
+        })
+        return true
     }
 }
